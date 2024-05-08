52ce9364ba8f509f46eb7c187076e34db79d4ed6
Aaron Gebremaraim <mulabr673@gmail.com>
2024-05-08 11:34:23 +0300

    two sum

--- 1.Two Sum/two_sum.py
+++ 1.Two Sum/two_sum.py
@@ -1,2 +1,13 @@
-#!usr/bin/ env/python3
-print('hello')
+#!/usr/bin/env python3
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mydict = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in mydict:
+                return [mydict[diff], i]
+            mydict[n] = i
+        return
