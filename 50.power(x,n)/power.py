8f4b8b0b369bb8db4eeffb7e9716455262174e93
Aaron Gebremaraim <mulabr673@gmail.com>
2024-05-11 12:20:08 +0300

    power imp.

---
+++ 50.power(x,n)/power.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n < 0:
+            x = 1/x
+            n = -n
+        temp = 1
+        while n > 0:
+            if n % 2 == 1:
+                temp *= x
+            x *= x
+            n //= 2
+        return temp
