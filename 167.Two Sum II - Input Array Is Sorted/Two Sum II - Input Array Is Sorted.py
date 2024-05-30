9d52bf77d84182fe6c74bec0e43071e8d75d8383
Aaron Gebremaraim <mulabr673@gmail.com>
2024-05-31 00:01:37 +0300

    Two sum II input Array is sorted

--- 167.Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.py
+++ 167.Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-       lPoint, rPoint = 0,len(numbers)-1
-       while lPoint < rPoint:
+        lPoint, rPoint = 0, len(numbers)-1
+        while lPoint < rPoint:
             tsum = numbers[lPoint] + numbers[rPoint]
             if tsum == target:
-                return [lPoint+1,rPoint+1]
+                return [lPoint+1, rPoint+1]
             elif tsum < target:
-                lPoint +=1
+                lPoint += 1
             else:
-                rPoint -=1
+                rPoint -= 1
