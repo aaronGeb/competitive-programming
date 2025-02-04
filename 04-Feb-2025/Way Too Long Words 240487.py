247338b1c56bcdc7e3cb143e6e195efd2c9f2dc8
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-04 18:12:16 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 04-Feb-2025/Way Too Long Words 240487.py
@@ -0,0 +1,9 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n =  int(input())
+for i in range(n):
+    s = input()
+    if len(s) > 10:
+        print(s[0] + str(len(s) - 2) + s[-1])
+    else:
+        print(s)
