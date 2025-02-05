cfcb7e1a6a079bcf8afd032c7da77cacce460807
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-05 12:28:42 +0300

    Created 'Check if All Characters Have Equal Number of Occurrences' by A2SV Hub

---
+++ 05-Feb-2025/Check if All Characters Have Equal Number of Occurrences 242413.py
@@ -0,0 +1,8 @@
+# Problem: Check if All Characters Have Equal Number of Occurrences - https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/description/
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        freq = Counter(s)
+        return len(set(freq.values()))==1
+
+        
