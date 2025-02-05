960ac0902ad7adc101aa9544aafcaa121dffe7c1
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-05 14:41:21 +0300

    Created 'Team' by A2SV Hub

---
+++ 05-Feb-2025/Team 242676.py
@@ -0,0 +1,16 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+def team(n):
+  count = 0
+  for _ in range(n):
+    a,b,c = map(int, input().split())
+    if a+b+c >= 2:
+      count += 1
+  return count
+
+
+
+#read the input
+if __name__ == '__main__':
+    n = int(input())
+    print(team(n))
