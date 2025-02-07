d05764f306366ad2d4c512ea43ea591bf94f9708
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-07 16:10:41 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 07-Feb-2025/Majority Element 249897.py
@@ -0,0 +1,11 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        for key,value in count.items():
+            if value > len(nums)/2:
+                return key
+        return
+
+        
