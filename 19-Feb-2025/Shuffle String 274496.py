e33c9a2c257f20f12017e8be987266c5b4552a00
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-19 17:15:03 +0300

    Created 'Shuffle String' by A2SV Hub

---
+++ 19-Feb-2025/Shuffle String 274496.py
@@ -0,0 +1,8 @@
+# Problem: Shuffle String - https://leetcode.com/problems/shuffle-string/description/
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = ['']* len(s)
+        for i,char in enumerate(s):
+            result[indices[i]] = char
+        return ''.join(result)
