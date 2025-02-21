b840fa72229bac90282becf6ec22554960182539
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-02-21 22:12:01 +0300

    Created 'Flipping an Image' by A2SV Hub

---
+++ 21-Feb-2025/Flipping an Image 279624.py
@@ -0,0 +1,5 @@
+# Problem: Flipping an Image - https://leetcode.com/problems/flipping-an-image/description/
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+         return [[1 - pixel for pixel in row[::-1]] for row in image]
