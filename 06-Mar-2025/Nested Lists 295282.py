8a4371b9e4a37c3f46be726d0a87128058854b97
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-06 22:28:51 +0300

    Created 'Nested Lists' by A2SV Hub

---
+++ 06-Mar-2025/Nested Lists 295282.py
@@ -0,0 +1,22 @@
+# Problem: Nested Lists - https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
+
+def second_min_names(data):
+    unique_results = sorted(set(result for _, result in data))  
+    if len(unique_results) < 2:
+        return None  
+    
+    second_min = unique_results[1]  
+
+    names = [name for name, result in data if result == second_min]
+    for _ in sorted(names):
+        print(_)
+
+if __name__ == '__main__':
+    data = []
+    for _ in range(int(input())):
+           
+            name = input()
+            score = float(input())
+            data.append([name, score])
+second_min_names(data)
+    
