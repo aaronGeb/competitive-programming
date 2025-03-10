1845ce4ad2b368af70858f08e245de3c914f1276
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-10 17:59:03 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300396.py
@@ -0,0 +1,17 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+def zoro():
+    t = int(input())  
+    for _ in range(t):
+        dt = input().strip() 
+        
+        if '<' in dt and '>' in dt:
+            print('?') 
+        elif '<' in dt:
+            print('<')  
+        elif '>' in dt:
+            print('>')  
+        else:
+            print('=')  
+if __name__ == "__main__":
+  zoro()
