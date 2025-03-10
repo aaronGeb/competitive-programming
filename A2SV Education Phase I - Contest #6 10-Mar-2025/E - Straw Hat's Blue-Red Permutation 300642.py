1e5a9d90953563a51a5c711155708e0622e3f11f
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-10 20:59:53 +0300

    Created 'E - Straw Hat's Blue-Red Permutation' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/E - Straw Hat's Blue-Red Permutation 300642.py
@@ -0,0 +1,35 @@
+# Problem: E - Straw Hat's Blue-Red Permutation - https://codeforces.com/gym/594356/problem/E
+
+def blue_red_permutation(n, a, s):
+    b = []
+    r = []
+    for i in range(n):
+        if s[i] == "B":
+            b.append(a[i])
+        else:
+            r.append(a[i])
+    b.sort()
+    r.sort()
+    t = sorted(range(1, n + 1))
+    bptr = 0
+    rptr = 0
+    for i in t:
+        if bptr < len(b) and b[bptr] >= i:
+            bptr += 1
+        elif rptr < len(r) and r[rptr] <= i:
+            rptr += 1
+        else:
+            return "NO"
+    return "YES"
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        s = input().strip()
+        print(blue_red_permutation(n, a, s))
+
+
+main()
