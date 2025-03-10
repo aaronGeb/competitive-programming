771c68447b1550536d86a854c2bf19b6472f64ce
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-10 17:58:56 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/B - Thousand Sunny's Network Setup 300395.py
@@ -0,0 +1,11 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+def network_setup(r,t):
+    a = list(map(int, input().split()))  
+    a.sort()
+    print(a[r - t])
+
+
+if __name__ == "__main__":
+  r, t = map(int, input().split()) 
+  network_setup(r,t)
