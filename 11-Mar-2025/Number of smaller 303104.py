1441c37e79c9d5152af92cbcda4361fb2dbc0724
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-11 21:17:01 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 11-Mar-2025/Number of smaller 303104.py
@@ -0,0 +1,13 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+import bisect
+def number_of_smaller():
+   n,m = map (int, input().split())
+   arr1 = list(map(int, input().split()))
+   arr2 = list(map(int, input().split()))
+   result = [bisect.bisect_left(arr1, x) for x in arr2]
+   print(*result)
+ 
+
+if __name__ == "__main__":
+    number_of_smaller()
