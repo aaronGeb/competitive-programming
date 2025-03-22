b11d22880de4537464ed111910b3240fbd61b308
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-22 15:18:19 +0300

    Created 'Third Maximum Number' by A2SV Hub

---
+++ 22-Mar-2025/Third Maximum Number 315551.py
@@ -0,0 +1,18 @@
+# Problem: Third Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        a = b=c = None
+        for num in nums:
+            if num in (a,b,c):
+                continue
+            if a is None or num > a:
+                b,c,a = a,b,num
+            elif b is None or num > b:
+                b,c = num, b
+            elif c is None or num > c:
+                c = num
+        return c if c is not None else a
+
+
+        
