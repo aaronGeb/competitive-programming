745f8a73cff2461615d1d15a1bfc93aedb8a9214
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-25 16:43:17 +0300

    Created 'Range Sum Query - Immutable' by A2SV Hub

---
+++ 25-Mar-2025/Range Sum Query - Immutable 317623.py
@@ -0,0 +1,17 @@
+# Problem: Range Sum Query - Immutable - https://leetcode.com/problems/range-sum-query-immutable/
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix_sum = list(accumulate(nums))
+        self.prefix_sum.append(0)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix_sum[right] - self.prefix_sum[left -1]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
