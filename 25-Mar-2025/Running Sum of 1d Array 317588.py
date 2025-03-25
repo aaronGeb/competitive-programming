c65ae88bf9e63020b3f13f9fe682d9539fe2d8c3
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-25 16:19:08 +0300

    Created 'Running Sum of 1d Array' by A2SV Hub

---
+++ 25-Mar-2025/Running Sum of 1d Array 317588.py
@@ -0,0 +1,11 @@
+# Problem: Running Sum of 1d Array - https://leetcode.com/problems/running-sum-of-1d-array/
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = []
+        s = 0
+        for num in nums:
+            s +=num
+            running_sum.append(s)
+        return running_sum
+        
