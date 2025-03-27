1e14562bf6ea1c9fb2d924358c9920b33c65c90c
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-03-27 17:34:40 +0300

    Created 'Fruit Into Baskets' by A2SV Hub

---
+++ 27-Mar-2025/Fruit Into Baskets 319773.py
@@ -0,0 +1,24 @@
+# Problem: Fruit Into Baskets - https://leetcode.com/problems/fruit-into-baskets
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = defaultdict(int)
+        left = 0  
+        max_fruits = 0
+
+        for right in range(len(fruits)):
+            basket[fruits[right]] += 1 
+
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1  # Shrink window
+
+            max_fruits = max(max_fruits, right - left + 1) 
+
+        return max_fruits
+
+
+# time complexity o(n)   Each fruit is processed at most twice
+# space coplexity O(1)  basket and count store at most 2 elements.
