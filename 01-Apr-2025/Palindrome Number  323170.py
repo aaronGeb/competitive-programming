9d44cef09e719f1bdafc672e254192facca1da9f
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-01 15:37:05 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 01-Apr-2025/Palindrome Number  323170.py
@@ -0,0 +1,6 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        return s== s[::-1]
