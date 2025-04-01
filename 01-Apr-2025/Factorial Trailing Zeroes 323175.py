395576569ba9f629908851737be2c84d6b19d9fc
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-01 15:40:08 +0300

    Created 'Factorial Trailing Zeroes' by A2SV Hub

---
+++ 01-Apr-2025/Factorial Trailing Zeroes 323175.py
@@ -0,0 +1,10 @@
+# Problem: Factorial Trailing Zeroes - https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n >=5:
+            count += n//5
+            n //=5
+        return count 
+        
