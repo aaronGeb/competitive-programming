74f62393d4b636a819898cf8013e23cfe48d2050
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-03 11:20:17 +0300

    Created 'F - Luffy’s Lineup Challenge' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 03-Apr-2025/F - Luffy’s Lineup Challenge 324540.py
@@ -0,0 +1,22 @@
+# Problem: F - Luffy’s Lineup Challenge - https://codeforces.com/gym/594356/problem/F
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int, input().split()))
+def next_index(start,target):
+    for i in range(start,n):
+        if b[i] == target:
+            return i
+swaps = []
+for i in range(n):
+    target = a[i]
+    right = next_index(i,target)
+ 
+    while right>i:
+        swaps.append([right,right+1])
+        b[right],b[right-1] = b[right-1],b[right]
+        right-=1
+count = 0
+print(len(swaps))
+for swap in swaps:
+    print(*swap)
