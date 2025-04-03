29d24e9f78c07e36ba1f9cc30071e343abc52c6d
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-03 11:21:13 +0300

    Created 'D - Pirates Island: Painting the Grand Line' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 03-Apr-2025/D - Pirates Island: Painting the Grand Line 324541.py
@@ -0,0 +1,25 @@
+# Problem: D - Pirates Island: Painting the Grand Line - https://codeforces.com/gym/594356/problem/D
+
+import sys 
+input = sys.stdin.readline 
+
+def solve():
+    n, m = map(int, input().split())
+    grid = [list(map(int, input().split())) for i in range(n)]
+    has_color = [0] * (n * m + 1)
+    adj_found= [0] * (n * m + 1)
+
+    for i in range(n):
+        for j in range(m):
+            has_color[grid[i][j]] = 1
+
+            if i + 1 < n and grid[i][j] == grid[i + 1][j]:
+                adj_found[grid[i][j]] = 1
+            if j + 1 < m and grid[i][j] == grid[i][j + 1]:
+                adj_found[grid[i][j]] = 1
+
+    print(sum(has_color) + sum(adj_found) - 1 - max(adj_found))
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        solve()
