7a6473b86d02ba50f8e70bf78f09afaab4e9ed95
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-04 11:07:40 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 04-Apr-2025/Sereja and Dima 325724.py
@@ -0,0 +1,25 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+cards = list(map(int, input().split()))
+
+left, right = 0, n - 1
+sereja_score, dima_score = 0, 0
+turn = 0 
+
+while left <= right:
+    if cards[left] > cards[right]:
+        chosen_card = cards[left]
+        left += 1
+    else:
+        chosen_card = cards[right]
+        right -= 1
+
+    if turn % 2 == 0:
+        sereja_score += chosen_card
+    else:
+        dima_score += chosen_card
+
+    turn += 1
+
+print(sereja_score, dima_score)
