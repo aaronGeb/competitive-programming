df2b0e76b70ebe323526e9a65817bef136ae7d99
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-04 10:54:46 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 04-Apr-2025/Kefa and Company 325716.py
@@ -0,0 +1,25 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+def factor(n, d, fr):
+
+    fr.sort()
+
+    mf = 0
+    cf = 0
+    left = 0  
+
+    for right in range(n):
+        cf += fr[right][1]  
+        while fr[right][0] - fr[left][0] >= d:
+            cf -= fr[left][1]  
+            left += 1
+        mf = max(mf, cf)
+
+    return mf
+
+# Read input
+n, d = map(int, input().split())
+fr = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Output the result
+print(factor(n, d, fr))
