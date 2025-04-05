03c05b0fac273a38935280754b27e33d777ffea5
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-05 11:04:57 +0300

    Created 'B - Mostly Red but Mostly Blue' by A2SV Hub

---
+++ A2SV Remote Contest #8 05-Apr-2025/B - Mostly Red but Mostly Blue 326761.py
@@ -0,0 +1,36 @@
+# Problem: B - Mostly Red but Mostly Blue - https://codeforces.com/gym/599884/problem/B
+
+import sys
+
+def can_paint_sequence(n, arr):
+    arr.sort()  
+   
+    red_sum = arr[-1]
+    red_count = 1
+    
+    blue_sum = arr[0]
+    blue_count = 1
+    
+    left = 1
+    right = n - 2  
+    
+    while left <= right:
+        blue_sum += arr[left]
+        blue_count += 1
+        left += 1
+        
+        if blue_count > red_count and blue_sum < red_sum:
+            return "YES"
+        
+        red_sum += arr[right]
+        red_count += 1
+        right -= 1
+
+    return "NO"
+
+# Read input
+t = int(sys.stdin.readline().strip())
+for _ in range(t):
+    n = int(sys.stdin.readline().strip())
+    arr = list(map(int, sys.stdin.readline().split()))
+    print(can_paint_sequence(n, arr))
