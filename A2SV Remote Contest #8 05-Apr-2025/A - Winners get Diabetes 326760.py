be950a65aaac36eb4ab2d5bed3a3b168c13851fe
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-05 11:04:44 +0300

    Created 'A - Winners get Diabetes' by A2SV Hub

---
+++ A2SV Remote Contest #8 05-Apr-2025/A - Winners get Diabetes 326760.py
@@ -0,0 +1,15 @@
+# Problem: A - Winners get Diabetes - https://codeforces.com/gym/599884/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    seen = set()
+    to = 0
+    for p in s:
+        if p not in seen:
+            to += 2
+            seen.add(p)
+        else:
+            to += 1
+    print(to)
