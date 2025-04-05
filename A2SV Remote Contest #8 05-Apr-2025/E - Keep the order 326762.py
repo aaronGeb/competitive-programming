754bdc2b42280dcfd3aafdb02a3f89621ab0e32d
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-05 11:05:18 +0300

    Created 'E - Keep the order' by A2SV Hub

---
+++ A2SV Remote Contest #8 05-Apr-2025/E - Keep the order 326762.py
@@ -0,0 +1,18 @@
+# Problem: E - Keep the order - https://codeforces.com/gym/599884/problem/E
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+p = {tp: idx for idx, tp in enumerate(b)}
+
+count = 0
+max_p = -1
+
+for trp in a:
+    cp = p[trp]
+    if cp < max_p:
+        count += 1
+    else:
+        max_p = cp
+
+print(count)
