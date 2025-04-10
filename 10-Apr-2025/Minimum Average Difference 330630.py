b0737ed456344c48b080ea8adf8859f2b4d0ba9f
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-10 15:50:12 +0300

    Created 'Minimum Average Difference' by A2SV Hub

---
+++ 10-Apr-2025/Minimum Average Difference 330630.py
@@ -0,0 +1,21 @@
+# Problem: Minimum Average Difference - https://leetcode.com/problems/minimum-average-difference/
+
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+        min_index = 0
+        min_diff =  float(inf)
+        n = len(nums)
+        for i in range(n):
+            left_sum += nums[i]
+            left_avg = left_sum//(i+1)
+            right_sum = total_sum - left_sum
+            right_avg = right_sum//(n-i-1) if (n-1-i) >0 else 0
+            avg_diff = abs(right_avg - left_avg)
+            if avg_diff < min_diff:
+                min_diff = avg_diff
+                min_index = i
+        return min_index
+
+        
