287ab1e4151df9d4d624b1ca4251e6c3638060a2
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-10 16:32:36 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 10-Apr-2025/Ehab Is an Odd Person 330647.py
@@ -0,0 +1,15 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+def process_array(arr):
+    has_odd = any(x % 2 == 1 for x in arr)
+    has_even = any(x % 2 == 0 for x in arr)
+
+    if has_odd and has_even:
+        arr.sort()
+    
+    print(*arr)
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    process_array(arr)
