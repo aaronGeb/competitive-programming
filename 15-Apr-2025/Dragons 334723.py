76a5a8eeb05bbaa7efd1df93cf5ae82141953145
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-15 22:10:36 +0300

    Created 'Dragons' by A2SV Hub

---
+++ 15-Apr-2025/Dragons 334723.py
@@ -0,0 +1,17 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+def can_kirito_win(s, dragons):
+    dragons.sort()
+    
+    for dragon_strength, bonus in dragons:
+        if s > dragon_strength:
+            s += bonus
+        else:
+            return "NO"
+    return "YES"
+
+# Input handling
+s, n = map(int, input().split())
+dragons = [tuple(map(int, input().split())) for _ in range(n)]
+
+print(can_kirito_win(s, dragons))
