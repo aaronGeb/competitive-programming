841b5f04b160e45edab5cbae8453442ff53fecb2
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-15 12:31:00 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 15-Apr-2025/Same Differences 334246.py
@@ -0,0 +1,31 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+from collections import defaultdict
+
+def count_pairs(t, test_cases):
+    results = []
+
+    for n, a in test_cases:
+        freq = defaultdict(int)
+        for i in range(n):
+            key = a[i] - i
+            freq[key] += 1
+
+        count = 0
+        for val in freq.values():
+            count += val * (val - 1) // 2
+
+        results.append(count)
+
+    return results
+
+
+t = int(input())
+test_cases = []
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    test_cases.append((n, a))
+
+for res in count_pairs(t, test_cases):
+    print(res)
