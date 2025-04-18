49fa05d0cacb64186440f6287cd3e755d032dce8
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-18 20:09:26 +0300

    Created 'F - The Limitation Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 18-Apr-2025/F - The Limitation Game 336929.py
@@ -0,0 +1,18 @@
+# Problem: F - The Limitation Game - https://codeforces.com/gym/603156/problem/F
+
+def main():
+    n, k = map(int, input().split())
+    a = input().strip()
+    b = input().strip()
+
+    ans = 0
+    tag = 1
+
+    for i in range(n):
+        tag = tag * 2 - (1 if a[i] == 'b' else 0) - (1 if b[i] == 'a' else 0)
+        tag = min(tag, k + 1)
+        ans += min(tag, k)
+
+    print(ans)
+
+main()
