e111fe5ae604efc4903bff74bdd93bfe75776389
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-21 09:24:54 +0300

    Created 'Can I Square?' by A2SV Hub

---
+++ 21-Apr-2025/Can I Square? 337997.py
@@ -0,0 +1,11 @@
+# Problem: Can I Square? - https://codeforces.com/contest/1915/problem/C
+
+import math
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    root = int(math.isqrt(total))
+    print("YES" if root * root == total else "NO")
