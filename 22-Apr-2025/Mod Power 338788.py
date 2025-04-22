7418b799b7a81ed6533fb5abcf67e2b8ea671efd
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-22 20:20:23 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 22-Apr-2025/Mod Power 338788.py
@@ -0,0 +1,8 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+print(a**b)
+print((a**b)%m)
