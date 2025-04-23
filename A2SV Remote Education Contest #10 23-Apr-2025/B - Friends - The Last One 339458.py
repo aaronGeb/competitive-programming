9f66d32cfb9d987461fb542d36ad22f1835dfef0
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-23 15:59:50 +0300

    Created 'B - Friends - The Last One' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 23-Apr-2025/B - Friends - The Last One 339458.py
@@ -0,0 +1,20 @@
+# Problem: B - Friends - The Last One - https://codeforces.com/gym/604857/problem/B
+
+def solve():
+    t  = int(input())
+    for _ in range(t):
+        n,m = map(int, input().split())
+        a = list(map(int, input().split()))
+        if n > m:
+            print('NO')
+            continue
+        a_sort =  sorted(a, reverse=True)
+        sum_plus = sum(a_sort) + n
+        max_a = a_sort[0]
+        min_a = a_sort[-1]
+        if sum_plus + max_a - min_a <=m:
+            print('YES')
+        else:
+            print('NO')
+if __name__ == '__main__':
+    solve()
