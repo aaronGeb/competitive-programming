4ad65c66beb7b9f9662bbcfdb3348073173f434a
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-24 09:27:49 +0300

    Created 'Valid Parentheses' by A2SV Hub

---
+++ 24-Apr-2025/Valid Parentheses 340111.py
@@ -0,0 +1,16 @@
+# Problem: Valid Parentheses - https://leetcode.com/problems/valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')':'(','}':'{',']':'['}
+        for char in s:
+            if char in mapping:
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack .append(char)
+
+        return True if not stack else False
