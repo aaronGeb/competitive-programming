adbd46a6558ad5c92cbf974694c2e0516bb849d2
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-28 19:10:14 +0300

    Created 'E - NATOLI's String Quest' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/E - NATOLI's String Quest 344118.py
@@ -0,0 +1,22 @@
+# Problem: E - NATOLI's String Quest - https://codeforces.com/gym/605795/problem/E
+
+s = input().strip()
+t = []
+u = []
+
+st = [None] * len(s)
+st[-1] = s[-1]
+for i in range(len(s) - 2, -1, -1):
+    st[i] = min(s[i], st[i + 1])
+
+idx = 0
+n = len(s)
+
+while idx < n or t:
+    if t and (idx == n or t[-1] <= st[idx]):
+        u.append(t.pop())
+    else:
+        t.append(s[idx])
+        idx += 1
+
+print(''.join(u))
