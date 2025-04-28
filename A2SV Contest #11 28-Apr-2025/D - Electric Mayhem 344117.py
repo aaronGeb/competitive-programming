338732ace6c9b7e37732aed8c05206052d5e0435
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-28 19:10:04 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344117.py
@@ -0,0 +1,13 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s = input()
+st = []
+for i in range(len(s)):
+    if st and s[i] == st[-1]:
+        st.pop()
+    else:
+        st.append(s[i])
+if st:
+    print("No")
+else:
+    print("Yes")
