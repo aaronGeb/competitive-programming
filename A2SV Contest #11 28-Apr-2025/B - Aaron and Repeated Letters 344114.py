8101cbf55525556cbd5f9d062bf1196159af066f
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-28 19:09:50 +0300

    Created 'B - Aaron and Repeated Letters' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/B - Aaron and Repeated Letters 344114.py
@@ -0,0 +1,12 @@
+# Problem: B - Aaron and Repeated Letters - https://codeforces.com/gym/605795/problem/B
+
+s = input().strip()
+st = []
+
+for c in s:
+    if st and st[-1] == c:
+        st.pop()  
+    else:
+        st.append(c) 
+
+print(''.join(st))
