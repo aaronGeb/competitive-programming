3972ee573b66b9b59cb17ce25e39bca4d73c040f
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-04-28 19:10:00 +0300

    Created 'C - Mr. Boomsy' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/C - Mr. Boomsy 344116.py
@@ -0,0 +1,12 @@
+# Problem: C - Mr. Boomsy - https://codeforces.com/gym/605795/problem/C
+
+t = int(input())
+for _ in range(t):
+    s = input().strip()
+    st = []
+    for c in s:
+        if st and ((st[-1] == 'A' and c == 'B') or (st[-1] == 'B' and c == 'B')):
+            st.pop()
+        else:
+            st.append(c)
+    print(len(st))
