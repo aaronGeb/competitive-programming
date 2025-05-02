0acfa314c329377729b8a32e54bf9e2f5d0739f6
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-02 17:07:17 +0300

    Created 'Print 1 to n without using loops' by A2SV Hub

---
+++ 02-May-2025/Print 1 to n without using loops 346941.py
@@ -0,0 +1,9 @@
+# Problem: Print 1 to n without using loops - https://www.geeksforgeeks.org/print-1-to-n-without-using-loops/
+
+def printNos(n):
+    if n > 0:
+        printNos(n - 1)
+        print(n, end=' ')
+
+n = 10
+printNos(n)
