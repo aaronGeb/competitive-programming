3303e8487b1cdf33739bcfdd054edf96b3970355
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-03 15:09:48 +0300

    Update LRU Cache 347584.py

--- 03-May-2025/LRU Cache 347584.py
+++ 03-May-2025/LRU Cache 347584.py
@@ -13,7 +13,6 @@ class LRUCache:
         self.cache.move_to_end(key)
         return self.cache[key]
         
-
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.cache.move_to_end(key)
@@ -27,3 +26,66 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+'''
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> node
+
+        # Dummy head and tail to avoid edge conditions
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        """Remove node from the doubly linked list."""
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_head(self, node):
+        """Insert node right after the dummy head."""
+        node.prev = self.head
+        node.next = self.head.next
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)        # move to head
+            self._add_to_head(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update value and move to front
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._add_to_head(node)
+        else:
+            if len(self.cache) == self.capacity:
+                # Evict least recently used (from tail)
+                lru = self.tail.prev
+                self._remove(lru)
+                del self.cache[lru.key]
+
+            # Add new node
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self._add_to_head(new_node)
+            
+'''
