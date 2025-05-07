39135a7e2084169065c1d9006a6fcf429ed5fb33
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-08 00:08:08 +0300

    Created 'Minimum Number of Operations to Move All Balls to Each Box' by A2SV Hub

---
+++ 07-May-2025/Minimum Number of Operations to Move All Balls to Each Box 350696.py
@@ -0,0 +1,18 @@
+# Problem: Minimum Number of Operations to Move All Balls to Each Box - https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        ans = [0] * n
+        cnt = 0
+        for i in range(1, n):
+            if boxes[i - 1] == '1':
+                cnt += 1
+            ans[i] = ans[i - 1] + cnt
+        cnt = s = 0
+        for i in range(n - 2, -1, -1):
+            if boxes[i + 1] == '1':
+                cnt += 1
+            s += cnt
+            ans[i] += s
+        return ans
