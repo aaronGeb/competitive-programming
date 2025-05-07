338d054b670e493054f1c6d675fcad81eb4224ac
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-07 16:48:59 +0300

    Created 'Find geometric sum of the series using recursion' by A2SV Hub

---
+++ 07-May-2025/Find geometric sum of the series using recursion 350050.py
@@ -0,0 +1,13 @@
+# Problem: Find geometric sum of the series using recursion - https://www.geeksforgeeks.org/find-geometric-sum-of-the-series-using-recursion/
+
+def sum(n):
+    
+    # base case 
+    if n == 0:
+        return 1
+    
+    return 1 / pow(3, n) + sum(n-1)
+
+n = 5;
+
+print(sum(n));
