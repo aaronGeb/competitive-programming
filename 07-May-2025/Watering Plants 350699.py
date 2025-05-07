f838083bff70bf35b5499d9a3c41b48a998aa444
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-08 00:13:26 +0300

    Created 'Watering Plants' by A2SV Hub

---
+++ 07-May-2025/Watering Plants 350699.py
@@ -0,0 +1,14 @@
+# Problem: Watering Plants - https://leetcode.com/problems/watering-plants/
+
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        ans, water = 0, capacity
+        for i, p in enumerate(plants):
+            if water >= p:
+                water -= p
+                ans += 1
+            else:
+                water = capacity - p
+                ans += i * 2 + 1
+        return ans
+        
