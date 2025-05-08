5e6420270da7f13054da083257af65652afe8c26
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-08 12:07:29 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 08-May-2025/X-Sum 350826.py
@@ -0,0 +1,26 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        board = [list(map(int, input().split())) for _ in range(n)]
+
+        diag1 = dict()  
+        diag2 = dict()  
+
+        for i in range(n):
+            for j in range(m):
+                diag1[i - j] = diag1.get(i - j, 0) + board[i][j]
+                diag2[i + j] = diag2.get(i + j, 0) + board[i][j]
+
+        max_sum = 0
+        for i in range(n):
+            for j in range(m):
+                total = diag1[i - j] + diag2[i + j] - board[i][j]
+                max_sum = max(max_sum, total)
+
+        print(max_sum)
+
+if __name__ == "__main__":
+  solve()
