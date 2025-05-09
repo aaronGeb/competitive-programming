5c15f471b450cc0411cfaadd011f476d6f5d7cb3
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-09 16:43:30 +0300

    Created 'Add Two Numbers II' by A2SV Hub

---
+++ 09-May-2025/Add Two Numbers II 351589.py
@@ -0,0 +1,31 @@
+# Problem: Add Two Numbers II - https://leetcode.com/problems/add-two-numbers-ii/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        stack1, stack2 = [], []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+
+        carry = 0
+        head = None
+
+        while stack1 or stack2 or carry:
+            total = carry
+            if stack1:
+                total += stack1.pop()
+            if stack2:
+                total += stack2.pop()
+
+            carry, val = divmod(total, 10)
+            head = ListNode(val, head)
+
+        return head
