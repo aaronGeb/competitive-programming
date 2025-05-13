597ddbf36f42be4601c93fc183958838f6eea655
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-13 18:51:56 +0300

    Created 'Shortest Subarray with Sum at Least K' by A2SV Hub

---
+++ 13-May-2025/Shortest Subarray with Sum at Least K 354453.py
@@ -0,0 +1,22 @@
+# Problem: Shortest Subarray with Sum at Least K - https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        prefix = [0] * (n + 1)
+
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+        dq = deque()
+        res = n + 1
+
+        for i in range(n + 1):
+            while dq and prefix[i] - prefix[dq[0]] >= k:
+                res = min(res, i - dq.popleft())
+
+            while dq and prefix[i] <= prefix[dq[-1]]:
+                dq.pop()
+
+            dq.append(i)
+
+        return res if res <= n else -1
