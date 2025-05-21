42029d81b5ce1fddaa355f884f17f1fca0df4bdf
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-21 09:20:18 +0300

    Created 'Helpful Maths' by A2SV Hub

---
+++ 21-May-2025/Helpful Maths 358557.py
@@ -0,0 +1,11 @@
+# Problem: Helpful Maths - https://codeforces.com/problemset/problem/339/A
+
+def xenia_sum(s: str) -> str:
+    digits = [int(ch) for ch in s if ch.isdigit()]
+  
+    digits.sort()
+    
+    return '+'.join(str(num) for num in digits)
+if __name__ == "__main__":
+    s = input().split(('+'))
+    print(xenia_sum(s))
