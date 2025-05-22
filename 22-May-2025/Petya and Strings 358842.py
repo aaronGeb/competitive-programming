376a311ff13d66a5fe2aaee0a3c3373047890eaa
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-05-22 09:53:27 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 22-May-2025/Petya and Strings 358842.py
@@ -0,0 +1,18 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+def compare_strings(s1: str, s2: str) -> int:
+    s1 = s1.lower()
+    s2 = s2.lower()
+
+    if s1 < s2:
+        return -1
+    elif s1 > s2:
+        return 1
+    else:
+        return 0
+
+
+s1 = input().strip()
+s2 = input().strip()
+
+print(compare_strings(s1, s2))
