7145e53ee2b644a35da55e4185e9c5db1b8ee861
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-06-07 11:44:40 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 07-Jun-2025/A. Beautiful Matrix 364468.py
@@ -0,0 +1,14 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+# Read the 5x5 matrix
+matrix = []
+for i in range(5):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+    for j in range(5):
+        if row[j] == 1:
+            x, y = i, j  # Position of '1'
+
+# Calculate Manhattan distance to center (2, 2)
+moves = abs(x - 2) + abs(y - 2)
+print(moves)
