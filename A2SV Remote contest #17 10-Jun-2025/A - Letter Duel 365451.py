11fdd5c734a9fb4f1d972e2d03698a5d16ca1910
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-06-10 23:17:30 +0300

    Created 'A - Letter Duel' by A2SV Hub

---
+++ A2SV Remote contest #17 10-Jun-2025/A - Letter Duel 365451.py
@@ -0,0 +1,14 @@
+# Problem: A - Letter Duel - https://codeforces.com/gym/614464/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    found = False
+    for i in range(n - 1):
+        if s[i] != s[i + 1]:
+            print(i + 1, i + 2)
+            found = True
+            break
+    if not found:
+        print(-1, -1)
