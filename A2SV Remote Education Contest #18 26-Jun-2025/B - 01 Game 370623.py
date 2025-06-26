3f71c6d9effb13ea0065f88649b16107871ee69e
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-06-26 10:22:47 +0300

    Created 'B - 01 Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #18 26-Jun-2025/B - 01 Game 370623.py
@@ -0,0 +1,12 @@
+# Problem: B - 01 Game - https://codeforces.com/gym/616066/problem/B
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    cnt0 = s.count('0')
+    cnt1 = s.count('1')
+    moves = min(cnt0, cnt1)
+    if moves % 2 == 1:
+        print("DA")
+    else:
+        print("NET")
