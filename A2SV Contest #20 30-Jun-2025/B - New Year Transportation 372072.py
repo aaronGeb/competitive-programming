f9e0e0786440d8aa150d3ef5b2770234e136ba48
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-06-30 20:10:57 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/B - New Year Transportation 372072.py
@@ -0,0 +1,10 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/618729/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+pos = 1
+while pos < t:
+    pos += a[pos - 1]
+
+print("YES" if pos == t else "NO")
