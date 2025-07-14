e2606ab828404925049d8f523545161eee2e05b4
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-14 09:31:18 +0300

    Created 'A - Swap to Max' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/A - Swap to Max 376361.py
@@ -0,0 +1,11 @@
+# Problem: A - Swap to Max - https://codeforces.com/gym/622136/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    for i in range(n):
+        if a[i] > b[i]:
+            a[i], b[i] = b[i], a[i]
+    print(max(a) * max(b))
