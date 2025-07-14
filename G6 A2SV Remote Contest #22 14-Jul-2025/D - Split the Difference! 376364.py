f02701293df45f4271c7cfce97bc939f8b915633
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-14 09:31:49 +0300

    Created 'D - Split the Difference!' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/D - Split the Difference! 376364.py
@@ -0,0 +1,11 @@
+# Problem: D - Split the Difference! - https://codeforces.com/gym/622136/problem/D
+
+n, k = map(int, input().split())
+A = list(map(int, input().split()))
+diff = []
+for i in range(1, n):
+    diff.append(A[i] - A[i - 1])
+diff.sort(reverse=True)
+ans = A[-1] - A[0]
+ans -= sum(diff[: k - 1])
+print(ans)
