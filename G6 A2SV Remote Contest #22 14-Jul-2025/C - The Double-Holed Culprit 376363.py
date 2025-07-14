1bd414a1f6232095132e73f21a0ce6fd86c041a1
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-14 09:31:44 +0300

    Created 'C - The Double-Holed Culprit' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/C - The Double-Holed Culprit 376363.py
@@ -0,0 +1,11 @@
+# Problem: C - The Double-Holed Culprit - https://codeforces.com/gym/622136/problem/C
+
+n = int(input())
+p = list(map(int, input().split()))
+for s in range(1, n + 1):
+    visited = set()
+    curr = s
+    while curr not in visited:
+        visited.add(curr)
+        curr = p[curr - 1]
+    print(curr, end=' ')
