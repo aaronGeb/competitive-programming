c49f577c96febbffac04e0c699dcf1929cb6d577
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-14 09:31:36 +0300

    Created 'B - Lifeboat Lineup' by A2SV Hub

---
+++ G6 A2SV Remote Contest #22 14-Jul-2025/B - Lifeboat Lineup 376362.py
@@ -0,0 +1,20 @@
+# Problem: B - Lifeboat Lineup - https://codeforces.com/gym/622136/problem/B
+
+n = int(input())
+r = []
+wo__chi = []
+men = []
+cap = []
+for _ in range(n):
+    name, status = input().split()
+    if status == "rat":
+        r.append(name)
+    elif status == "woman" or status == "child":
+        wo__chi.append(name)
+    elif status == "man":
+        men.append(name)
+    elif status == "captain":
+        cap.append(name)
+
+for name in r + wo__chi + men + cap:
+    print(name)
