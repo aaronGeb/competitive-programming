91b1d1c42bbce52fdb4ca637aa25fe7511198ce3
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-28 09:36:23 +0300

    Created 'A - Verifying Access Keys' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/A - Verifying Access Keys 379945.py
@@ -0,0 +1,26 @@
+# Problem: A - Verifying Access Keys - https://codeforces.com/gym/625306/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    dig = []
+    letter = []
+    is_valid = True
+
+    for ch in s:
+        if ch.isdigit():
+            if letter: 
+                is_valid = False
+                break
+            dig.append(ch)
+        else:
+            letter.append(ch)
+
+    if is_valid:
+        if dig != sorted(dig) or letter != sorted(letter):
+            is_valid = False
+
+    print("YES" if is_valid else "NO")
