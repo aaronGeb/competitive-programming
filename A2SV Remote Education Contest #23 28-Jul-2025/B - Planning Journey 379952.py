a0a1d46b9dd927a232fa0049fcce2bfd0510c7b6
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-28 10:59:42 +0300

    Created 'B - Planning Journey' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/B - Planning Journey 379952.py
@@ -0,0 +1,26 @@
+# Problem: B - Planning Journey - https://codeforces.com/gym/625306/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    cnt = 0
+    i = 0
+    ones = sum(a[:k])
+
+    while i <= n - k:
+        if ones == 0:
+            cnt += 1
+            i += k + 1
+            if i <= n - k:
+                ones = sum(a[i:i + k])
+        else:
+          
+            if a[i] == 1:
+                ones -= 1
+            if i + k < n and a[i + k] == 1:
+                ones += 1
+            i += 1
+
+    print(cnt)
