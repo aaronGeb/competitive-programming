138a612ecfd4242c05ccfec6c2d9c6cca8bb4a97
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-28 10:59:38 +0300

    Created 'C - Sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 28-Jul-2025/C - Sort 379951.py
@@ -0,0 +1,18 @@
+# Problem: C - Sort - https://codeforces.com/gym/625306/problem/C
+
+t = int(input())
+for _ in range(t):
+    n, c = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    costs = [a[i] + i + 1 for i in range(n)] 
+    costs.sort()
+    
+    used = 0
+    for cost in costs:
+        if c >= cost:
+            c -= cost
+            used += 1
+        else:
+            break
+    print(used)
