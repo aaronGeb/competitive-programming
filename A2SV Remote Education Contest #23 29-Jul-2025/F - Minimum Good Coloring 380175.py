9f60a9537f0a4bc5065eb88812d4eb42e29d3914
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-07-29 07:51:15 +0300

    Created 'F - Minimum Good Coloring' by A2SV Hub

---
+++ A2SV Remote Education Contest #23 29-Jul-2025/F - Minimum Good Coloring 380175.py
@@ -0,0 +1,40 @@
+# Problem: F - Minimum Good Coloring - https://codeforces.com/gym/625306/problem/F
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+edges = []
+ 
+for i in range(m):
+    u, v = map(int, input().split())
+    u -= 1
+    v -= 1
+    graph[u].append((v, i))
+    edges.append((u, v))
+ 
+color = [0] * m
+visited = [0] * n
+need_two_colors = False
+ 
+def dfs(u):
+    global need_two_colors
+    visited[u] = 1
+    for v, idx in graph[u]:
+        if visited[v] == 0:
+            color[idx] = 1
+            dfs(v)
+        elif visited[v] == 1:
+            color[idx] = 2
+            need_two_colors = True
+        else:
+            color[idx] = 1
+    visited[u] = 2
+ 
+for i in range(n):
+    if visited[i] == 0:
+        dfs(i)
+ 
+if need_two_colors:
+    print(2)
+else:
+    print(1)
+print(*color)
