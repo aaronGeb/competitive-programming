d24c323584b05d13f1a111ccee6e796d44da3d59
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-09 13:19:29 +0300

    Created 'B - Make Product Equal One' by A2SV Hub

---
+++ A2SV Contest #24 09-Aug-2025/B - Make Product Equal One 383105.py
@@ -0,0 +1,19 @@
+# Problem: B - Make Product Equal One - https://codeforces.com/gym/626626/problem/B
+
+n = int(input())
+a = list(map(int, input().split()))
+cost = 0
+neg = 0
+z = 0
+for i in a:
+    if i == 0:
+        cost += 1
+        z += 1
+    elif i > 0:
+        cost += i - 1
+    else:
+        cost += -1 - i
+        neg += 1
+if neg % 2 == 1 and z == 0:
+    cost += 2
+print(cost)
