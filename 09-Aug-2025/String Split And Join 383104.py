b734ef58d25d90d108fb33e3793d958332ae23b0
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-09 13:17:15 +0300

    Created 'String Split And Join' by A2SV Hub

---
+++ 09-Aug-2025/String Split And Join 383104.py
@@ -0,0 +1,12 @@
+# Problem: String Split And Join - https://www.hackerrank.com/challenges/python-string-split-and-join?isFullScreen=true
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(' ')
+    return "-".join(line)
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
