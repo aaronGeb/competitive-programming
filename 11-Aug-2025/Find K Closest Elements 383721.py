987192126824e25694bea4579140e60ab55c830c
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-11 23:19:06 +0300

    Created 'Find K Closest Elements' by A2SV Hub

---
+++ 11-Aug-2025/Find K Closest Elements 383721.py
@@ -0,0 +1,12 @@
+# Problem: Find K Closest Elements - https://leetcode.com/problems/find-k-closest-elements/
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr)-1
+        while right - left +1 > k:
+            if abs(arr[left] - x) > abs(arr[right]-x):
+                left +=1
+            else:
+                right -=1
+        return arr[left: right+1]
