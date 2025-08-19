7defe53bf58437bc1e9025ec7ffc527e717e2946
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-19 09:40:17 +0300

    Created 'Permutation Sequence' by A2SV Hub

---
+++ 19-Aug-2025/Permutation Sequence 386749.py
@@ -0,0 +1,16 @@
+# Problem: Permutation Sequence - https://leetcode.com/problems/permutation-sequence/description/
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+
+        numbers = list(range(1, n + 1))
+        k -= 1  
+        result = []
+
+        for i in range(n, 0, -1):
+            fact = math.factorial(i - 1)
+            index = k // fact
+            result.append(str(numbers.pop(index)))
+            k %= fact
+
+        return ''.join(result)
