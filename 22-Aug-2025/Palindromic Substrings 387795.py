6cfd76eb03909d8e37943c75ab29e97a57542141
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-22 16:39:24 +0300

    Created 'Palindromic Substrings' by A2SV Hub

---
+++ 22-Aug-2025/Palindromic Substrings 387795.py
@@ -0,0 +1,13 @@
+# Problem: Palindromic Substrings - https://leetcode.com/problems/palindromic-substrings/description/
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for center in range(len(s)):
+            for right in [center, center+1]:
+                left = center
+                while left >=0 and right < len(s) and s[left] == s[right]:
+                    count +=1
+                    left -=1
+                    right +=1
+        return count
