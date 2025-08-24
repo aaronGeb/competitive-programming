a08b698b1b2dd1ac9fe64cd53d79ff3f389a64e4
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-24 20:37:23 +0300

    Created 'Maximum XOR of Two Numbers in an Array' by A2SV Hub

---
+++ 24-Aug-2025/Maximum XOR of Two Numbers in an Array 388339.py
@@ -0,0 +1,14 @@
+# Problem: Maximum XOR of Two Numbers in an Array - https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
+
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        max_xor = 0
+        mask = 0
+        for i in range(31, -1, -1):
+            mask |= (1 << i)
+            prefixes = {num & mask for num in nums}  
+            candidate = max_xor | (1 << i)
+            if any((candidate ^ p) in prefixes for p in prefixes):
+                max_xor = candidate
+
+        return max_xor
