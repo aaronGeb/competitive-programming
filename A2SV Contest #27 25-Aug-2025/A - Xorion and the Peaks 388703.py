3fb8f0b45d8245f8dbfde1f610061143fb0b3fd6
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-25 18:19:06 +0300

    Created 'A - Xorion and the Peaks' by A2SV Hub

---
+++ A2SV Contest #27 25-Aug-2025/A - Xorion and the Peaks 388703.py
@@ -0,0 +1,17 @@
+# Problem: A - Xorion and the Peaks - https://codeforces.com/gym/630556/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    max_peaks = (n - 1) // 2
+    
+    if k > max_peaks:
+        print(-1)
+        continue
+    
+    arr = list(range(1, n + 1))
+    
+    for i in range(1, k + 1):
+        arr[2*i - 1], arr[2*i] = arr[2*i], arr[2*i - 1]
+    
+    print(" ".join(map(str, arr)))
