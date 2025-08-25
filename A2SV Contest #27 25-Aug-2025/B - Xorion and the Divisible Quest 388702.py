081f1dc3d66947c493c72064066e749001796940
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-25 18:19:01 +0300

    Created 'B - Xorion and the Divisible Quest' by A2SV Hub

---
+++ A2SV Contest #27 25-Aug-2025/B - Xorion and the Divisible Quest 388702.py
@@ -0,0 +1,13 @@
+# Problem: B - Xorion and the Divisible Quest - https://codeforces.com/gym/630556/problem/B
+
+import math
+
+n = int(input())
+arr = list(map(int, input().split()))
+g = arr[0]
+for x in arr[1:]:
+    g = math.gcd(g, x)
+if g in arr:
+    print(g)
+else:
+    print(-1)
