33d26dc86ac83a7258af8f86dc2f3f0e9ccc0d5c
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-08-29 10:21:32 +0300

    Created 'C - Xorion: The Subsegment Showdown' by A2SV Hub

---
+++ A2SV Contest #27 29-Aug-2025/C - Xorion: The Subsegment Showdown 389811.py
@@ -0,0 +1,15 @@
+# Problem: C - Xorion: The Subsegment Showdown - https://codeforces.com/gym/630556/problem/C
+
+test = int(input())
+for _ in range(test):
+    x, y = map(int, input().split())
+    
+    diff = x ^ y
+    ans = 1
+    
+    
+    while diff & 1 == 0 and diff != 0:
+        ans <<= 1
+        diff >>= 1
+    
+    print(ans)
