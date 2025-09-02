e7eaeb3e156e024d01cd441f5fbee9962290944b
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-02 09:25:23 +0300

    Created 'B - Loopix: Circle of Truth' by A2SV Hub

---
+++ A2SV Contest #28 02-Sep-2025/B - Loopix: Circle of Truth 390781.py
@@ -0,0 +1,12 @@
+# Problem: B - Loopix: Circle of Truth - https://codeforces.com/gym/632067/problem/B
+
+def solve():
+    s = input()
+    n_count = s.count('N')
+    if n_count == 1:
+        print("NO")
+    else:
+        print("YES")
+t = int(input())
+for _ in range(t):
+    solve()
