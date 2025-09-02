8ca8cb8b868238c2f800346a39c688d91547c9f3
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-02 09:24:55 +0300

    Created 'A - Loopix: Multiples Hunt' by A2SV Hub

---
+++ A2SV Contest #28 02-Sep-2025/A - Loopix: Multiples Hunt 390778.py
@@ -0,0 +1,33 @@
+# Problem: A - Loopix: Multiples Hunt - https://codeforces.com/gym/632067/problem/A
+
+import sys
+
+def solve():
+    l, r, k = map(int, sys.stdin.readline().split())
+
+    low = 0
+    high = r - l + 1
+    ans = 0
+
+    while low <= high:
+        m = low + (high - low) // 2
+
+        if m == 0:
+            low = m + 1
+            continue
+        last_num = l + m - 1
+        if last_num <= r and (r // last_num) >= k:
+            ans = m
+            low = m + 1
+        else:
+            high = m - 1
+
+    print(ans)
+
+
+try:
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        solve()
+except (IOError, ValueError):
+    pass
