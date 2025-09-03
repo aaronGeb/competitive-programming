9ec1b6cacf6f75cf4293f01a89c0e1c1cba23ad4
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-03 10:10:13 +0300

    Created '132 Pattern' by A2SV Hub

---
+++ 03-Sep-2025/132 Pattern 391037.py
@@ -0,0 +1,13 @@
+# Problem: 132 Pattern - https://leetcode.com/problems/132-pattern/
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        i = -inf
+        stack = []
+        for j in nums[::-1]:
+            if j < i:
+                return True
+            while stack and stack[-1] < j:
+                i = stack.pop()
+            stack.append(j)
+        return False
