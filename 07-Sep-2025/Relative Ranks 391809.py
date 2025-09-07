2677b965ea174f88433c49e2b71952e5855b871a
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-07 21:48:46 +0300

    Created 'Relative Ranks' by A2SV Hub

---
+++ 07-Sep-2025/Relative Ranks 391809.py
@@ -0,0 +1,18 @@
+# Problem: Relative Ranks - https://leetcode.com/problems/relative-ranks/description/
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        indexed_scores = list(enumerate(score))  
+        indexed_scores.sort(key=lambda x: -x[1])
+        result = [""] * len(score)
+        for rank, (i, s) in enumerate(indexed_scores):
+            if rank == 0:
+                result[i] = "Gold Medal"
+            elif rank == 1:
+                result[i] = "Silver Medal"
+            elif rank == 2:
+                result[i] = "Bronze Medal"
+            else:
+                result[i] = str(rank + 1)
+        
+        return result
