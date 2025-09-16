ed2bb93bf6034bf9fcd43ece595f5ae26d9c35b5
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-16 09:40:14 +0300

    Created 'B. Petr and a Combination Lock' by A2SV Hub

---
+++ 16-Sep-2025/B. Petr and a Combination Lock 393456.py
@@ -0,0 +1,22 @@
+# Problem: B. Petr and a Combination Lock - https://codeforces.com/contest/1097/problem/B
+
+import sys
+
+def main():
+    data = list(map(int, sys.stdin.read().strip().split()))
+    n = data[0]
+    a = data[1:]
+    for mask in range(1 << n):
+        total = 0
+        for i in range(n):
+            if (mask >> i) & 1:
+                total += a[i]
+            else:
+                total -= a[i]
+        if total % 360 == 0:
+            print("YES")
+            return
+    print("NO")
+
+if __name__ == "__main__":
+    main()
