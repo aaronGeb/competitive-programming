da3f25955cffe044ef851dd861c59beeb8e0cd42
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-17 10:49:43 +0300

    Created 'Reducing Dishes' by A2SV Hub

---
+++ 17-Sep-2025/Reducing Dishes 393645.py
@@ -0,0 +1,12 @@
+# Problem: Reducing Dishes - https://leetcode.com/problems/reducing-dishes/
+
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort(reverse=True)
+        ans = s = 0
+        for x in satisfaction:
+            s += x
+            if s <= 0:
+                break
+            ans += s
+        return ans
