eaf10918e05143ad153c309013ae6544738485f8
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-20 12:59:03 +0300

    Created 'C - Binary Deque' by A2SV Hub

---
+++ A2SV Contest #29 20-Sep-2025/C - Binary Deque 394400.py
@@ -0,0 +1,31 @@
+# Problem: C - Binary Deque - https://codeforces.com/gym/633600/problem/C
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, s = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    l = 0
+    cur = 0
+    best = -1  
+
+    for r in range(n):
+        cur += arr[r]
+        
+        while l <= r and cur > s:
+            cur -= arr[l]
+            l += 1
+        if cur == s:
+            best = max(best, r - l + 1)
+
+    if best == -1:
+        if s == 0:
+            
+            print(n)
+        else:
+            print(-1)
+    else:
+        print(n - best)
