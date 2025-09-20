da8fcb38c4bf9efd571a7dc82656903cad7c9ad4
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-20 12:56:22 +0300

    Created 'B - Game of Mathletes' by A2SV Hub

---
+++ A2SV Contest #29 20-Sep-2025/B - Game of Mathletes 394399.py
@@ -0,0 +1,31 @@
+# Problem: B - Game of Mathletes - https://codeforces.com/gym/633600/problem/B
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    cnt = [0] * (n + 1)
+    for x in arr:
+        if 1 <= x <= n:
+            cnt[x] += 1
+
+    score = 0
+    for v in range(1, (k // 2) + 1):
+        u = k - v
+        if u < 1 or u > n:
+            continue
+        if u == v:
+            continue
+        if v < u:
+            score += min(cnt[v], cnt[u])
+
+    if k % 2 == 0:
+        mid = k // 2
+        if 1 <= mid <= n:
+            score += cnt[mid] // 2
+
+    print(score)
