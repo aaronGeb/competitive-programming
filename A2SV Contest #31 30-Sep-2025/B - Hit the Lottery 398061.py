d98ef792e02da957a07af73c47a9acd41a902730
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-30 20:02:37 +0300

    Created 'B - Hit the Lottery' by A2SV Hub

---
+++ A2SV Contest #31 30-Sep-2025/B - Hit the Lottery 398061.py
@@ -0,0 +1,9 @@
+# Problem: B - Hit the Lottery - https://codeforces.com/gym/637748/problem/B
+
+n = int(input())
+bills = [100, 20, 10, 5, 1]
+count = 0
+for bill in bills:
+    count += n // bill
+    n %= bill
+print(count)
