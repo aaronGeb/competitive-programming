69a7a6a8fe26f9aad0c2808be3ed85303252ae81
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-09-30 20:02:50 +0300

    Created 'A - In Search of an Easy Problem' by A2SV Hub

---
+++ A2SV Contest #31 30-Sep-2025/A - In Search of an Easy Problem 398062.py
@@ -0,0 +1,13 @@
+# Problem: A - In Search of an Easy Problem - https://codeforces.com/gym/637748/problem/A
+
+import sys
+
+data = sys.stdin.read().strip().split()
+if not data:
+    sys.exit()
+n = int(data[0])
+arr = data[1 : n + 1]
+if "1" in arr:
+    print("HARD")
+else:
+    print("EASY")
