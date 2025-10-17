533883465ce57cb8efec5ece41346492143f8045
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-10-17 09:02:36 +0300

    Created 'All Paths From Source to Target' by A2SV Hub

---
+++ 17-Oct-2025/All Paths From Source to Target 403912.py
@@ -0,0 +1,16 @@
+# Problem: All Paths From Source to Target - https://leetcode.com/problems/all-paths-from-source-to-target/
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        n = len(graph)
+        q = deque([[0]])
+        ans = []
+        while q:
+            path = q.popleft()
+            u = path[-1]
+            if u == n - 1:
+                ans.append(path)
+                continue
+            for v in graph[u]:
+                q.append(path + [v])
+        return ans
