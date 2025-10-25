4526fa3829a699e5e316d47d0b4f97a7e5836db7
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-10-25 16:53:15 +0300

    Created 'Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ 25-Oct-2025/Team Composition: Programmers and Mathematicians 405682.py
@@ -0,0 +1,12 @@
+# Problem: Team Composition: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+mport sys
+ 
+data = list(map(int, sys.stdin.read().strip().split()))
+t = data[0]
+out = []
+idx = 1
+for _ in range(t):
+    a = data[idx]; b = data[idx+1]; idx += 2
+    out.append(str(min((a + b) // 4, a, b)))
+print("\n".join(out))
