842c805ddb9ad138629667090bcc1bf223eea2c7
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-10-26 10:57:34 +0300

    Created 'Possible Bipartition' by A2SV Hub

---
+++ 26-Oct-2025/Possible Bipartition 405781.py
@@ -0,0 +1,23 @@
+# Problem: Possible Bipartition - https://leetcode.com/problems/possible-bipartition/
+
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        color = {}
+
+        def dfs(node, c):
+            if node in color:
+                return color[node] == c
+            color[node] = c
+            return all(dfs(nei, 1 - c) for nei in graph[node])
+        
+        for person in range(1, n + 1):
+            if person not in color:
+                if not dfs(person, 0):
+                    return False
+        return True
