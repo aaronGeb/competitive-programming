0ebed15caf94ff58fe83f98b680ed9c1f46638d2
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-10-28 08:44:16 +0300

    Created 'B. Ehab Is an Odd Person' by A2SV Hub

---
+++ 28-Oct-2025/B. Ehab Is an Odd Person 406082.py
@@ -0,0 +1,16 @@
+# Problem: B. Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+def smallest_array(n, a):
+    has_even = any(x % 2 == 0 for x in a)
+    has_odd = any(x % 2 == 1 for x in a)
+ 
+    if has_even and has_odd:
+        return sorted(a)
+    else:
+        return a
+ 
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int, input().split()))
+    result = smallest_array(n, a)
+    print(*result)
