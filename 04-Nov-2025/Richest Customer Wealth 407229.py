f81d537c5ec7defc3b09641cca0a9d9881fd5626
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-11-04 09:03:19 +0300

    Created 'Richest Customer Wealth' by A2SV Hub

---
+++ 04-Nov-2025/Richest Customer Wealth 407229.py
@@ -0,0 +1,5 @@
+# Problem: Richest Customer Wealth - https://leetcode.com/problems/richest-customer-wealth/description/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(sum(v) for v in accounts)
