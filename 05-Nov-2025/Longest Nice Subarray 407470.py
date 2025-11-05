75689d065cd6b1555c83f0140617424962e4c34e
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-11-05 13:42:04 +0300

    Created 'Longest Nice Subarray' by A2SV Hub

---
+++ 05-Nov-2025/Longest Nice Subarray 407470.py
@@ -0,0 +1,19 @@
+# Problem: Longest Nice Subarray - https://leetcode.com/problems/longest-nice-subarray/
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+    
+        left = 0
+        bitmask = 0
+        max_length = 0
+
+        for right in range(len(nums)):
+    
+            while (bitmask & nums[right]) != 0:
+                bitmask ^= nums[left]  # Remove nums[left] from the bitmask
+                left += 1  # Move left pointer forward
+            bitmask |= nums[right]
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+    
