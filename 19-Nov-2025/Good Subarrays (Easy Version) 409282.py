484db1fa13d80b7022977657c83553919e169c89
Aaron  GEBREMARIAM <71099880+aaronGeb@users.noreply.github.com>
2025-11-19 09:53:01 +0300

    Created 'Good Subarrays (Easy Version)' by A2SV Hub

---
+++ 19-Nov-2025/Good Subarrays (Easy Version) 409282.py
@@ -0,0 +1,22 @@
+# Problem: Good Subarrays (Easy Version) - https://codeforces.com/problemset/problem/1736/C1
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    mn = float("inf")
+    ans = 0
+
+    for i in range(n):
+        c = a[i] - (i + 1)
+        mn = min(mn, c)
+        l_min = max(1, 1 - mn)
+
+        ans += (i + 1) - l_min + 1
+
+    print(ans)
